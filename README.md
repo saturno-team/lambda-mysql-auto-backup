af86974a68ebe87c829bf505ce9ee3af89929019
icleitoncosta <cleitoncosta83@gmail.com>
2025-02-17 20:18:04 -0300

    feat: Criado função para testes

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # lambda-mysql-auto-backup
 Function to be executed inside lambda to perform mysql backups
+
+## How to use
+0. Clone this repository
+1. Open Secrets Manager in you AWS Console
+2. Click in Add new secret
+3. Choose Credentials for another database
+4. Put your user and password for your database
+5. Choose MySQL in Database type
+6. Fill Server Address, name of your database and port
+7. Configure your .env file or pass in Github actions (like I do) (set all values of inside .env file)
+8. Go to IAM > Users > (your-user) > Add Permissions > Attach Policies Directly.
+9. Create a Custom IAM Policy. Use the policy below to grant access to the specific secret:
+```
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowGetSpecificSecret",
+      "Effect": "Allow",
+      "Action": "secretsmanager:GetSecretValue",
+      "Resource": "arn:aws:secretsmanager:us-east-1:991856824586:secret:YOURSECRETNAME)-*"
+    }
+  ]
+}
+```
+
+10. Create a function in AWS Lampda with name and set secret in Github secrets with LAMBDA_FUNCTION_NAME
---
+++ index.js
@@ -0,0 +1,86 @@
+import mysqldump from 'mysqldump';
+import dotenv from "dotenv";
+import { v4 as uuidv4 } from 'uuid';
+import aws from 'aws-sdk';
+import {
+    SecretsManagerClient,
+    GetSecretValueCommand,
+  } from "@aws-sdk/client-secrets-manager";
+
+export const handler = async (event) => {
+  try {
+    dotenv.config();
+    const credentials = await getDatabaseCredentials();
+    console.log('Credentials recovered');
+
+    const filename = `cleiton_backup_mysql_${new Date().toJSON().slice(0, 10)}_${Math.random().toString(36).slice(2, 10)}.sql`;
+
+    const { dump } = await mysqldump({
+      connection: {
+        host: credentials.host,
+        user: credentials.username,
+        password: credentials.password,
+        database: credentials.dbname,
+      },
+    });
+    console.log('Backup generated');
+    console.log('Uploading...');
+
+    await uploadFile(
+      dump.data, 'text/sql', {
+      key: filename,
+      bucket: process.env.AWS_BUCKET,
+    });
+    console.log('Backup successful!');
+    
+  } catch (error) {
+    console.log(error);
+  }
+  const response = {
+    statusCode: 200,
+  };
+  return response;
+};
+
+async function getDatabaseCredentials() {
+    try {
+        const client = new SecretsManagerClient({
+            region: process.env.AWS_REGION,
+        });
+        const response = await client.send(
+        new GetSecretValueCommand({
+            SecretId: process.env.SECRET_NAME,
+            VersionStage: "AWSCURRENT",
+        })
+        );
+        return JSON.parse(response.SecretString);
+    } catch (error) {
+        console.error("Error in search secret:", error);
+        throw new Error("Error on search credentials");
+    }
+}
+
+async function uploadFile(
+    buffer,
+    contentType,
+    options
+  ){
+    try {
+      const client = new aws.S3();
+
+      const idUpload = uuidv4();
+      const params = {
+        Bucket: options?.bucket ? options.bucket : 'uploads',
+        Key: options?.key ? options.key : idUpload,
+        Body: buffer,
+        ContentType: contentType,
+        ACL: options?.acl,
+      };
+      return await client.upload(params).promise();
+    } catch (error) {
+      logger.error(error);
+      return error;
+    }
+  }
+
+handler();
